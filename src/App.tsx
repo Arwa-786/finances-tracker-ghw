257151c1ed5dd6b52064fc066e296bf97f882394
Arwa-786 <arwaa0521@gmail.com>
2025-12-26 19:04:28 -0500

    Did the CSS for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {useState} from "react";
 import styles from "./App.module.css";
 
+type View = "dashboard" | "transactions" | "add";
+
 function App() {
-  return <h1 className={styles.heading}>Finance Tracker</h1>;
+  const [currentView, setCurrentView] =  useState<View>("dashboard");
+  return (
+  <main className={styles.app}>
+    <nav className={styles.nav}>
+      <div className={styles.navBrand}>
+        <span className={styles.logo}>💰
+        </span>
+         <h1 className={styles.navTitle}>Finance Tracker</h1>
+      </div>
+    </nav>
+  </main>  
+  );
 }
 
 export default App;
